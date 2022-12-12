a943e29db4be815a030baefa335005baed25b932
Owy Ebute <bedrockresources21@gmail.com>
2022-12-12 04:53:43 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Main - Main Block
+ * Description: print all single digit number of base 10
+ * starting from 0, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}	
+
