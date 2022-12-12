83ea722ec2564bf4bd3f9097546236d692d689a0
Owy Ebute <bedrockresources21@gmail.com>
2022-12-12 10:08:41 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * Main - Main Block
+ * Description: Prints all single digit numbers of base 10
+ * startin from 0, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a');
+	{
+		putchar(c);
+		c--;
+
+	}
+
+	putchar('\n');
+	return (0);
+}
